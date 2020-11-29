ff4dca3af8f6589f343b1b8389e18f45caa32e7a
Digpal Singh Rana <digplrana35@gmail.com>
2020-11-29 12:24:16 +0530

    Added Header component and links to Shop Page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import {Route} from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 import Homepage from './pages/homepage/Homepage.component';
 import ShopPage from './pages/shoppage/shop.component.jsx';
+import Header from './components/header/header.component';
 
 import './App.css';
 
@@ -9,8 +10,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Route exact path='/' component={Homepage} />
-      <Route path='/shop' component={ShopPage} />
+      <Header />
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
